778b47043075cbb6eafcc57756664ab437649a1e
kaustubkarki <kaustubkarki@gmail.com>
2026-05-28 10:56:26 +0545

    [feat]: standard changes.

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>hello ishnat</Text>
+      <Text style={styles.text}>Hello, so you are back?</Text>
+      <Text
+        style={{
+          textAlign: "center",
+          marginTop: 16,
+          fontSize: 18,
+          fontWeight: "500",
+        }}
+      >
+        Let me tell you something for now
+      </Text>
+      <Text style={{ textAlign: "center", marginTop: 8, fontSize: 16 }}>
+        My partner accused me of having no sense of direction.
+      </Text>
+      <Text style={{ textAlign: "center", marginTop: 4, fontSize: 16 }}>
+        I got so angry, I packed up my stuff and right.
+      </Text>
     </View>
   );
 }
@@ -11,13 +27,18 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
-    color: '#000000',
+    color: "#000000",
     fontSize: 28,
-    fontWeight: '600',
+    fontWeight: "600",
+  },
+  subtext: {
+    color: "#000000",
+    fontSize: 22,
+    fontWeight: "600",
   },
 });
